68908d25dadb2a7be6c7f5aca9ee2d149af1e563
ez-709 <egor.zhuravlev0407@gmail.com>
2025-07-13 02:34:00 +0300

    написан базовый функционал для работы с json

--- programm/main.py
+++ programm/main.py
@@ -1,23 +1,29 @@
+import os
+
+from storage import json_to_py, find_satellites, active_names, create_urls_to_htpp, write_or_update_tles
 from tracking.parsing import get_not_deb_tle
 from tracking.calculation import calculate_orbit
 from tracking.visualization import visualization_orbit_for_one_satellite
 
-url_noaa = "https://celestrak.org/NORAD/elements/gp.php?NAME=NOAA&FORMAT=TLE"
-#url_meteor = "https://celestrak.org/NORAD/elements/gp.php?NAME=NOAA&FORMAT=TLE"
-#url_iss = "https://celestrak.org/NORAD/elements/gp.php?NAME=NOAA&FORMAT=TLE"
-noaa_active_names = ['NOAA 15', 'NOAA 18', 'NOAA 19', 'NOAA 20', 'NOAA 21']
 
-tles = get_not_deb_tle(url_noaa, noaa_active_names)
+#пример кода для обновления tle или добавления новых
+'''tles = []
+urls = create_urls_to_htpp()
 
-"""tles = [
-    ['NOAA 15', '1 25338U 98030A   25192.50463084  .00000165  00000+0  85569-4 0  9998', '2 25338  98.5338 216.8655 0008997 265.1501  94.8653 14.26994957412957'],
-    ['NOAA 18', '1 28654U 05018A   25192.47314934  .00000051  00000+0  50383-4 0  9991', '2 28654  98.8422 271.5906 0014877  42.7816 317.4511 14.13617971 38255'],
-    ['NOAA 19', '1 33591U 09005A   25192.41396440  .00000145  00000+0  10138-3 0  9990', '2 33591  98.9961 257.2952 0012671 273.8547  86.1176 14.13387759846601'],
-    ['NOAA 20 (JPSS-1)', '1 43013U 17073A   25192.48730641  .00000027  00000+0  33532-4 0  9999', '2 43013  98.7410 130.6541 0001302 129.6058 230.5233 14.19544787396139'],
-    ['NOAA 21 (JPSS-2)', '1 54234U 22150A   25191.74794225  .00000018  00000+0  29250-4 0  9996', '2 54234  98.7274 130.1716 0002751  84.6320 275.5170 14.19543300138104']
-]
-"""
 
-print(tles)
-sat_inf = calculate_orbit(tles[0], 4, 4000)
-visualization_orbit_for_one_satellite(sat_inf, 37.6155600, 55.7522200, print_time = False)
+for url in urls:
+    tles.append(get_not_deb_tle(url, active_names()))
+
+for tle in tles:
+    write_or_update_tles(tle)'''
+cd = os.getcwd() 
+cd_sat = os.path.join(cd, 'programm', 'data', 'data_base', 'satellites.json')
+cd_tle = os.path.join(cd, 'programm', 'data', 'data_base', 'tle.json')
+cd_processing = os.path.join(cd, 'programm', 'data', 'data_base', 'processing.json')
+
+tles = json_to_py(cd_tle)
+print(tles["NOAA 15"])
+
+
+'''sat_inf = calculate_orbit(tles[0], 4, 4000)
+visualization_orbit_for_one_satellite(sat_inf, 37.6155600, 55.7522200, print_time = False)'''
--- programm/storage.py
+++ programm/storage.py
@@ -8,7 +8,7 @@ cd_sat = os.path.join(cd, 'programm', 'data', 'data_base', 'satellites.json')
 cd_tle = os.path.join(cd, 'programm', 'data', 'data_base', 'tle.json')
 cd_processing = os.path.join(cd, 'programm', 'data', 'data_base', 'processing.json')
 
-def json_to_list_of_dicts(cd_json):
+def json_to_py(cd_json):
     '''Читает json с именем 'name_json" и возвращаtn один единый словарь или список словарей'''
     with open(cd_json, 'r', encoding='utf-8') as file:
         data = json.load(file)
@@ -19,7 +19,7 @@ def find_satellites(name=None, frequency=None, min_record_time=None, signal_type
     Фильтрует спутники по заданным параметрам
     Возвращает список словарей с подходящими спутниками
     '''
-    sat_data = json_to_list_of_dicts(cd_sat)
+    sat_data = json_to_py(cd_sat)
     res = sat_data 
     
     if name is not None:
@@ -39,9 +39,21 @@ def find_satellites(name=None, frequency=None, min_record_time=None, signal_type
     
     return res
 
+def active_names():
+    """
+    функция возвращает список всех спутников из файла satellites.json
+    """
+    sat_data = json_to_py(cd_sat)
+    names = []
+
+    for sat in sat_data:
+        names.append(sat["name"])
+    
+    return names
+
 
 def create_urls_to_htpp():
-    sat_data = json_to_list_of_dicts(cd_sat)
+    sat_data = json_to_py(cd_sat)
     base = "https://celestrak.org/NORAD/elements/gp.php?NAME="
     form = "&FORMAT=TLE"
 
@@ -56,31 +68,27 @@ def create_urls_to_htpp():
 
     return urls
 
-
-tles = [
-    ['NOAA 15', '1 25338U 98030A   25192.50463084  .00000165  00000+0  85569-4 0  9998', '2 25338  98.5338 216.8655 0008997 265.1501  94.8653 14.26994957412957'],
-    ['NOAA 18', '1 28654U 05018A   25192.47314934  .00000051  00000+0  50383-4 0  9991', '2 28654  98.8422 271.5906 0014877  42.7816 317.4511 14.13617971 38255'],
-    ['NOAA 19', '1 33591U 09005A   25192.41396440  .00000145  00000+0  10138-3 0  9990', '2 33591  98.9961 257.2952 0012671 273.8547  86.1176 14.13387759846601'],
-    ['NOAA 20 (JPSS-1)', '1 43013U 17073A   25192.48730641  .00000027  00000+0  33532-4 0  9999', '2 43013  98.7410 130.6541 0001302 129.6058 230.5233 14.19544787396139'],
-    ['NOAA 21 (JPSS-2)', '1 54234U 22150A   25191.74794225  .00000018  00000+0  29250-4 0  9996', '2 54234  98.7274 130.1716 0002751  84.6320 275.5170 14.19543300138104']
-]
-
-
-def write_or_update_tles(tles):
+def write_or_update_tles(new_tles):
     '''
     на вход в функцию поступает список tles
     результатом работы функции является обновленный tle.json
     '''
-    tle_json = json_to_list_of_dicts(cd_tle)
+    try:
+        tle_json = json_to_py(cd_tle)
+    except:
+        tle_json = {}
 
-    for sat_tle in tles:
+    for sat_tle in new_tles:
         if sat_tle[0] not in tle_json:
             tle_json[sat_tle[0]] = sat_tle
     
     for sat in tle_json:
-        if tle_json[sat] 
-            
+        for tle in new_tles:
+            if sat == tle[0]:
+                if tle_json[sat][0] != tle[1] or tle_json[sat][1] != tle[2]:
+                    tle_json[sat] = [tle[1], tle[2]]
+    
+    with open(cd_tle, 'w') as f:
+        json.dump(tle_json, f, indent=4)
 
-    return 
 
-print(write_or_update_tles(tles))
