a0d877d1af3e9a0209367caebf5d4d355ca47834
Georgiy <egor.zhuravlev04@yandex.ru>
2025-10-30 22:17:49 +0300

    исправил лог

--- programm/test.py
+++ programm/test.py
@@ -1,14 +1,6 @@
-import time
+from decode.decoder_apt import decoder_apt
 
-from decode.decoding_procesing import record_and_decode_satellite
-from tracking.utils import unix_to_utc
-
-time_start = time.time()
-print(unix_to_utc(time_start))
-record_and_decode_satellite('NOAA 18', 12 * 60)
-time_finish = time.time()
-print('\n')
-print(unix_to_utc(time_finish))
-print('\n')
-print(time_finish - time_start)
-
+decoder_apt(
+    r"D:\my-files\my_projects\satellite-apt-decode\programm\data_decode\NOAA_18__2025-10-30_16-16-18_+3_UTC.wav",
+    r"D:"
+)
